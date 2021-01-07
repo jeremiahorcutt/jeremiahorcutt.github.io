772c46bd64fbf62e4f0fdd5f0fac5f36f513a11a
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-01-07 13:33:38 -0700

    a

---
+++ week2/w2.js
@@ -0,0 +1,6 @@
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("ye").textContent = year;
+
+let modified = document.lastModified;
+document.getElementById("mod").textContent = modified;
