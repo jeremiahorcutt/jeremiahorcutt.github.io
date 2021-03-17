5ccbed87aa5e14808986048e2152418c56d304b8
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-03-17 16:19:53 -0600

    a

---
+++ lesson11/js/eventsss.js
@@ -0,0 +1,21 @@
+// json file load
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  
+    const towns = jsonObject['towns'];
+    var i = 0;
+    var name = towns[i].name;
+
+    for (i = 0; i < towns.length; i++) {
+        var name = towns[i].name;
+        if (name == "Soda Springs") {
+            var event = towns[i].events.join('<br>');
+            document.getElementById("events").innerHTML = event;
+        }
+    }
+});
