48c14f5da0f457e23da0ca9d9dd9200c8047255a
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-03-19 20:12:49 -0600

    a

--- finalproject/js/main.js
+++ finalproject/js/main.js
@@ -1,49 +1,3 @@
-// json file load reviews
-const requestreviews = 'reviews.json';
-
-fetch(requestreviews)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  
-    const reviews = jsonObject['review'];
-    var a, b, c;
-    a = Math.round(Math.random() * 10);
-    b = Math.round(Math.random() * 10);
-    if (b == a){
-        b =  Math.round(Math.random() * 10);
-    }
-    c = Math.round(Math.random() * 10);
-    if (c == a | c == b){
-        c = Math.round(Math.random() * 10);
-    }
-    var index = [a, b, c];
-    for (let i = 0; i < index.length; i++ ) {
-        let review = document.createElement('div');
-        review.setAttribute("id","review" + i);
-        review.setAttribute("class","review");
-
-        let h2 = document.createElement('h2');
-        h2.setAttribute("class","reviewname")
-
-        let comment = document.createElement('p');
-        comment.setAttribute("class","comment")
-
-        let rate = document.createElement('h2');
-        rate.setAttribute("class","rate");
-
-
-h2.textContent = reviews[index[i]].name;
-comment.textContent = "'" + reviews[index[i]].comment + "'";
-rate.textContent = reviews[index[i]].rate; 
-
-review.appendChild(rate);
-review.appendChild(h2);
-review.appendChild(comment);
-document.querySelector('div#reviews').appendChild(review);
-    }
-      });
 
 //menu
 const menubutton = document.querySelector('.menu');
@@ -56,51 +10,48 @@ menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive
 //forecast
 
 let request = new XMLHttpRequest();
-const apiURLF = "https://api.openweathermap.org/data/2.5/forecast?q=Cozumel&units=imperial&appid=46c7dd86b763384946891a3bda107a7f";
-request.open('Get', apiURLF, true);
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=45.6770&lon=-111.0429&exclude=hourly&appid=46c7dd86b763384946891a3bda107a7f&units=imperial";
+request.open('Get', apiURL, true);
 request.send();
 
 request.onload = function () {
     var data = JSON.parse(request.responseText);
     console.log(data);
     var weatherLogo = "https://openweathermap.org/img/wn/";
-    const forecastArr = data.list;
+    const forecastArr = data['daily'];
     let j = 0;
     for (var i = 0; i < forecastArr.length; i++) {
-        var dt = forecastArr[i].dt_txt;
-        var time = dt.includes('18:00:00');
-        if (time == true && j < 3){
-            var temperature = data.list[i].main.temp;
+        let temp = forecastArr[i].temp;
+        if (j < 3){
+            var temperature = forecastArr[i].temp.day;
             console.log(temperature);
             document.getElementById(j + "Cast").innerHTML = temperature;
-            document.getElementById(j + "Img").setAttribute("src",weatherLogo + data.list[i].weather[0].icon + "@2x.png");
-            document.getElementById(j + "Img").setAttribute("alt",data.list[i].weather[0].description);
+            document.getElementById(j + "Img").setAttribute("src",weatherLogo + forecastArr[i].weather[0].icon + "@2x.png");
+            document.getElementById(j + "Img").setAttribute("alt",forecastArr[i].weather[0].description);
             j++;
         }
     }
 };
 
 //weather
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Cozumel&units=imperial&appid=46c7dd86b763384946891a3bda107a7f";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    const weather = jsObject['weather'];
-    const main =jsObject['main'];
-    const wind =jsObject['wind'];
-    document.getElementById('temp').textContent = main.temp;
-    document.getElementById('humid').textContent = main.humidity;
-    document.getElementById('speed').textContent = wind.speed;
+    const current = jsObject['current'];
+    const weather = current.weather;
+    document.getElementById('temp').textContent = current.temp;
+    document.getElementById('humid').textContent = current.humidity;
+    document.getElementById('speed').textContent = current.wind_speed;
     document.getElementById('weat').textContent = weather[0].main;
-    doInputOutput(main, wind);
+    doInputOutput(current);
   });
 
   
 
-function doInputOutput(main, wind){
-    let tempF = main.temp;
-    let speed = wind.speed;
+function doInputOutput(current){
+    let tempF = current.temp;
+    let speed = current.wind_speed;
     let calculatedChill = windChill(tempF, speed);
     windChill(tempF, speed);
     document.getElementById('chill').innerHTML = calculatedChill;
