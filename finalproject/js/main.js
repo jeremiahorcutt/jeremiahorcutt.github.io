9a05bf34d0ef51a7f4bbfb918adba170caae64c5
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-03-22 14:48:08 -0600

    a

--- finalproject/js/main.js
+++ finalproject/js/main.js
@@ -131,3 +131,19 @@ document.getElementById('1day').innerHTML = oneday;
 document.getElementById('2day').innerHTML = twoday;
 document.getElementById('3day').innerHTML = threeday;
 
+//form validation
+window.onload = function()
+{
+  document.getElementById("submit").onclick = validateForm;
+};
+function validateForm(){
+    let name = document.getElementById('name');
+    let phone = document.getElementById('phone');
+    let zip = document.getElementById('zip');
+     if((name.checkValidity()) && (phone.checkValidity()) && (zip.checkValidity())) {
+        window.location.href = "thanks.html";
+     }
+    else{
+        return null;
+    }
+};
